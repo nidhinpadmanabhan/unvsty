da6ff73b34de1ed9db00be6ea84cd642e84c634e
NIDHIN P <nidhinp45@gmail.com>
2023-05-27 16:47:34 +0530

    third stage

--- scpr.js
+++ scpr.js
@@ -12,6 +12,7 @@ function reload() {
         <table class="bg-dark text-white w-100 border rounded p-5 mt-4">
             <thead>
                 <tr>
+                    <th class="c6 c7">NO</th>
                     <th class="c6">UNIVERSITY NAME</th>
                     <th class="c6">UNIVERSITY WEBSITE</th>
                     <th class="c6 c9">DOMAIN</th>
@@ -28,15 +29,18 @@ function reload() {
 
 function displayPlace() {
     place = pname.value
-    fetch(`http://universities.hipolabs.com/search?country=${place}`).then(data => data.json()).then(data => displayData(data))
+    fetch(`http://universities.hipolabs.com/search?country=${place}`).then(data => data.json()).then(data => displayData(data)).catch(e => displayError())
 }
 
 
 function displayData(pData) {
     cname.innerHTML = pData[0].country
+    let n=0
     for (i of pData) {   
+        n++
         result1.innerHTML += `
         <tr>
+            <td class='c6 c7 border text-start ps-2'>${n}</td>
             <td class='c6 border text-start ps-2'>${i.name}</td>
             <td class='c6 border text-start ps-2'><a style="text-decoration: none;" href="${i.web_pages}">${i.web_pages}</a></td>
             <td class='c6  c9 border text-start ps-2'>${i.domains}</td>
@@ -45,6 +49,17 @@ function displayData(pData) {
         
     }
 
+
+}
+
+function displayError(){
+    result.innerHTML = `
+
+    <div class="text-light c10 container p-3 text-center mt-5" style = "background-color: rgba(0, 0, 0, 0.738); border-radius: 10px; border: white 1px solid;" >
+        <div>
+            <h3 class="c40 pt-3" ><strong> check if the spelling of the country is correct?? </strong></h3>
+        </div>
+    </div> `
 
 }
 
